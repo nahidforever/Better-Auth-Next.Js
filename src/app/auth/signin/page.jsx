5ee70b42bffef51fd8ad2f589aebbfc6fe853698
Nahid1828 <isam15-5334@diu.edu.bd>
2026-04-23 11:02:10 +0600

    Build login page and handle credentials sign in

---
+++ src/app/auth/signin/page.jsx
@@ -0,0 +1,100 @@
+"use client";
+
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  InputGroup,
+  Label,
+  TextField,
+} from "@heroui/react";
+
+import { Check, Eye, EyeSlash } from "@gravity-ui/icons";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+
+const SignIPage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const userData = Object.fromEntries(formData.entries());
+
+    console.log("From submitted with", userData);
+
+    const { data, error } = await authClient.signIn.email({
+      email: userData.email,
+      password: userData.password,
+      rememberMe: true,
+      callbackURL: "/",
+    });
+
+    console.log("Sign In response:", { data, error });
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <h2 className="text-2xl font-bold text-center">Please Sign in</h2>
+
+      <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+        {/* email */}
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input name="email" placeholder="Your Email Address" />
+          <FieldError />
+        </TextField>
+        <TextField className="w-full max-w-[280px]" name="password">
+          <Label>Password</Label>
+          <InputGroup>
+            <InputGroup.Input
+              className="w-full max-w-[280px]"
+              type={isVisible ? "text" : "password"}
+              name="password"
+              placeholder="Your Password"
+            />
+            <InputGroup.Suffix className="pr-0">
+              <Button
+                isIconOnly
+                aria-label={isVisible ? "Hide password" : "Show password"}
+                size="sm"
+                variant="ghost"
+                onPress={() => setIsVisible(!isVisible)}
+              >
+                {isVisible ? (
+                  <Eye className="size-4" />
+                ) : (
+                  <EyeSlash className="size-4" />
+                )}
+              </Button>
+            </InputGroup.Suffix>
+          </InputGroup>
+        </TextField>
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default SignIPage;
